a4a0fc1b2edcecc96c38e10b630af6b8cf696df9
Dusan Janosik <dusan@djanosik.cz>
2015-08-24 23:10:24 +0200

    Makes it possible to use WebDeploy to publish DNX web applications.

--- template/fake.fsx
+++ template/fake.fsx
@@ -2,6 +2,15 @@
 #load "build/FAKEX/tools/fakex.fsx"
 open Fake
 
-// custom targets
+// Target "Deploy" (fun _ ->
+//     WebDeploy { 
+//         appPath = "[IisAppPath]";
+//         project = "src/[ProjectName]/project.json";
+//         serviceUrl = "[MsDeployServiceUrl]";
+//         skipExtraFiles = false;
+//         userName = "[UserName]";
+//         password = (environVar "WEBDEPLOY_PWD")
+//     }
+// )
 
 RunTargetOrDefault "Build"
--- tools/fakex.fsx
+++ tools/fakex.fsx
@@ -1,15 +1,28 @@
+[<AutoOpen>] 
+module Fakex
+
 open Fake
 open System
 open System.IO
 
+// types
+
+type WebDeployArgs = { appPath:string; project:string; serviceUrl:string; skipExtraFiles:bool; userName:string; password:string }
+
 // functions 
 
 let inline FileName fullName = Path.GetFileName fullName
-let version = if buildServer <> BuildServer.LocalBuild then buildVersion else "1.0.0"
 
 let dnvm args =
     ExecProcessWithLambdas (fun info ->
         info.FileName <- __SOURCE_DIRECTORY__ + "\dnvm.cmd"
+        info.Arguments <- args
+    ) TimeSpan.MaxValue true failwith traceImportant
+        |> ignore
+        
+let msdeploy args =
+    ExecProcessWithLambdas (fun info ->
+        info.FileName <- ProgramFilesX86 + "\IIS\Microsoft Web Deploy V3\msdeploy.exe"
         info.Arguments <- args
     ) TimeSpan.MaxValue true failwith traceImportant
         |> ignore
@@ -25,7 +38,7 @@ let BackupProject project =
         
 let UpdateVersion version project =
     log ("Updating version in " + project)   
-    ReplaceInFile (fun s -> s.Replace("1.0.0-ci", version)) project
+    ReplaceInFile (fun s -> replace "1.0.0-ci" version s) project
     
 let BuildProject project =
     dnu ("pack --configuration Release " + (DirectoryName project))
@@ -42,24 +55,34 @@ let RestoreProject backup =
         
 let RunTests project =
     dnx ("\"" + (DirectoryName project) + "\" test")    
- 
+
+let WebDeploy (args:WebDeployArgs) =
+    let outDirectory = FullName "artifacts/publish"
+    dnu ("publish '" + (DirectoryName (FullName args.project)) + "' --out '" + outDirectory + "' --configuration Release --no-source --runtime active --wwwroot-out 'wwwroot'")
+    
+    msdeploy ("-source:IisApp='" + outDirectory + "\wwwroot' -dest:IisApp='" + args.appPath + "',ComputerName='https://" + args.serviceUrl + "/msdeploy.axd',UserName='" + args.userName 
+        + "',Password='" + args.password + "',IncludeAcls='False',AuthType='Basic' -verb:sync -enableLink:contentLibExtension -retryAttempts:2" 
+        + if args.skipExtraFiles then " -enableRule:DoNotDeleteRule" else "" + " -disablerule:BackupRule")
 
 // targets
     
 Target "Clean" (fun _ ->
-    !! "artifacts" ++ "**/bin"
+    !! "artifacts" ++ "src/*/bin" ++ "test/*/bin"
         |> DeleteDirs
 )
 
 Target "BackupProjects" (fun _ ->
-    !! "**/project.json" ++ "**/project.lock.json"
+    !! "src/*/project.json" ++ "src/*/project.lock.json" ++ "test/*/project.json" ++ "test/*/project.lock.json"
         |> Seq.iter(BackupProject)
         
     ActivateFinalTarget "RestoreProjects"
 )
 
 Target "UpdateVersions" (fun _ ->    
-    !! "**/project.json"
+    let version = if buildServer <> BuildServer.LocalBuild 
+        then buildVersion else "1.0.0"
+    
+    !! "src/*/project.json" ++ "test/*/project.json"
         |> Seq.iter(UpdateVersion version)
 )
 
@@ -68,22 +91,22 @@ Target "RestoreDependencies" (fun _ ->
 )
 
 Target "BuildProjects" (fun _ ->
-    !! "src/**/project.json" 
+    !! "src/*/project.json" 
         |> Seq.iter(BuildProject)
 )
 
 Target "CopyArtifacts" (fun _ ->    
-    !! "src/**/*.nupkg" 
+    !! "src/*/bin/**/*.nupkg" 
         |> Seq.iter(CopyArtifact)
 )
 
 Target "RunTests" (fun _ ->
-    !! "test/**/project.json" 
+    !! "test/*/project.json" 
         |> Seq.iter(RunTests)
 )
 
 FinalTarget "RestoreProjects" (fun _ ->
-    !! "**/project.json.bak" ++ "**/project.lock.json.bak"
+    !! "src/*/project.json.bak" ++ "src/*/project.lock.json.bak" ++ "test/*/project.json.bak" ++ "test/*/project.lock.json.bak"
         |> Seq.iter(RestoreProject)
 )
 
